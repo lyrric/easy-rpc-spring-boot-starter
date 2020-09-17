7d71bd0b127e33e052c8f58c53437e61c1d7f6ff
lyrric <xx20510@163.com>
2020-09-17 21:07:15 +0800

    修改了客户端netty的启动时间

--- client-starter/src/main/java/com/github/easy/rpc/client/RpcClientConfiguration.java
+++ client-starter/src/main/java/com/github/easy/rpc/client/RpcClientConfiguration.java
@@ -3,6 +3,8 @@ package com.github.easy.rpc.client;
 import com.github.easy.rpc.client.model.RpcClientProperties;
 import com.github.easy.rpc.client.service.NettyService;
 import lombok.extern.apachecommons.CommonsLog;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -21,20 +23,21 @@ import javax.annotation.Resource;
 @ComponentScan(basePackages = "com.github.easy.rpc.client")
 @EnableScheduling
 @CommonsLog
-public class RpcClientConfiguration {
+public class RpcClientConfiguration implements ApplicationRunner {
 
     @Resource
     private RpcClientProperties rpcClientProperties;
     @Resource
     private NettyService nettyService;
+
+
     /**
      * 初始化netty
      * @return
      * @throws InterruptedException
      */
-    @PostConstruct
-    public void initNetty() throws InterruptedException {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
         nettyService.init(rpcClientProperties);
     }
-
 }
